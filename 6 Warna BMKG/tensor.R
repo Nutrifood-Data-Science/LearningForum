e9332256eddd0ea52d67fae02cc5aef982124f38
ikanx101 <ikanx101@gmail.com>
2025-10-28 06:45:37 +0000

    save hasil model keras

--- 6 Warna BMKG/tensor.R
+++ 6 Warna BMKG/tensor.R
@@ -1,5 +1,5 @@
-rm(list=ls())
-gc()
+# rm(list=ls())
+# gc()
 
 library(dplyr)
 library(caret)
@@ -7,6 +7,8 @@ library(tensorflow)
 library(keras3)
 library(readxl)
 library(scales)
+
+setwd("~/LearningForum/6 Warna BMKG")
 
 # kita ambil selling out
 file = "data so ns polos_281025.xlsx"
@@ -44,8 +46,8 @@ df_input =
   merge(df_so,df_biru,all.x = T) %>% 
   merge(df_hijau) %>% 
   merge(df_merah) %>% 
-  select(-tgl) 
-  # mutate(so = scale(so))
+  select(-tgl) %>% 
+  mutate(so = rescale(so))
 
 # kita pisahkan
 # target
@@ -58,32 +60,36 @@ model = keras_model_sequential()
 
 model %>%
   # Adds a densely-connected layer with 64 units to the model:
-  layer_dense(units = 30, activation = 'sigmoid', 
+  layer_dense(units = 21, activation = 'gelu', 
               input_shape = ncol(input)) %>%
   # Add another:
-  layer_dense(units = 30, activation = 'sigmoid') %>%
-  layer_dense(units = 30, activation = 'relu') %>%
-  layer_dense(units = 30, activation = 'sigmoid') %>%
-  layer_dense(units = 1, activation = 'relu')
+  layer_dense(units = 12, activation = 'gelu') %>%
+  layer_dense(units = 19, activation = 'tanh') %>%
+  layer_dense(units = 86, activation = 'gelu') %>%
+  layer_dense(units = 12, activation = 'gelu') %>%
+  layer_dense(units = 2, activation = 'swish') %>%
+  layer_dense(units = 1, activation = 'sigmoid')
 
 # optimizer
 model %>% compile(
   loss = "mse",
   optimizer =  "adam",
-  metrics = list("mean_absolute_error")
+  metrics = list(metric_r2_score()) # Add R2Score as a metric
+  # metrics = list("mean_absolute_error")
 )
 
 model %>% summary()
 
 model %>% fit(input,target, 
-              epochs = 500,
+              epochs = 3000,
               validation_split = 0.3,
-              batch_size = 30)
+              batch_size = 45)
 
 scores = model %>% evaluate(input, 
                             target, verbose = 0)
 print(scores)
 
+# r2 hasil training 0.7538
 
 # kita buat prediksinya
 y_deep = model %>% predict(input)
@@ -97,16 +103,66 @@ df_hasil    = data.frame(y = target,y_deep = y_deep)
 df_hasil |>
   ggplot(aes(x = y,y = y_deep)) +
   # plot awal fungsi dari soal
-  geom_point(group = 1,color = "blue") +
-  geom_smooth(method = "loess") +
+  geom_point(group = 1,color = "blue") 
+  # geom_smooth(method = "loess") 
   # xlim(10,28) +
   # ylim(10,28) +
-  coord_equal()
 
 
+df_hasil %>% 
+  mutate(error = abs(y_deep - y)) %>% 
+  summarise(mean(error),
+            median(error))
+
+setwd("~/LearningForum/6 Warna BMKG/saved_model/model_1")
+keras3::save_model(model,"model.keras")
+
+library(DALEX)
+
+explainer_mmm = DALEX::explain(model = model,
+                               data  = input,
+                               y     = target,
+                               type  = "regression",
+                               label = "Regression BMKG",
+                               colorize = FALSE)
+
+performa_model = model_performance(explainer_mmm)
+performa_model
+
+var_importante  = model_parts(explainer_mmm)
+plot_importance = plot(var_importante,show_boxplots = FALSE)
+plot_importance
+
+mp_mmm = model_profile(explainer_mmm, 
+                       variable =  c("average_blue_dominance","average_green_dominance",
+                                     "average_red_dominance"), 
+                       type = "accumulated")
+plot(mp_mmm)
 
 
+max_so = 
+  df_input %>% 
+  filter(so == max(so)) %>% 
+  select(-so) %>% 
+  as.matrix()
+max_plot = predict_parts(explainer_mmm, max_so)
+plot(max_plot)
 
+min_so = 
+  df_input %>% 
+  filter(so == min(so)) %>% 
+  select(-so) %>% 
+  as.matrix()
+min_plot = predict_parts(explainer_mmm, min_so)
+plot(min_plot)
 
-
-
+setwd("~/LearningForum/6 Warna BMKG")
+save(input,
+     target,
+     df_input,
+     explainer_mmm,
+     performa_model,
+     var_importante,plot_importance,mp_mmm,
+     max_so,max_plot,
+     min_so,min_plot,
+     file = "hasil_keras.rda")
